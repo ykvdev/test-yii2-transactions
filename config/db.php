6a62e002e01b077f9c92dbbaff78da347e10f3c6
atoumus <atoumus@gmail.com>
2019-11-30 10:27:17 +0200

    Install Yii

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=test_starkkraft',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
